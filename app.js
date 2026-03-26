a72269c4a8b1e1e2084606ef24d4883bbaa5fcea
Ashwani-sahu87 <105841839+Ashwani-sahu87@users.noreply.github.com>
2026-03-26 20:49:51 +0530

    fix: DB error

--- app.js
+++ app.js
@@ -11,10 +11,11 @@ dotenv.config({ path: './.env'});
 const app = express();
 
 const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.DB_PORT
 });
 
 
